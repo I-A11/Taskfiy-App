315fb09fb8f541cc49dd9f61ee185ce507d6a5ef
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-12-07 19:40:30 +1100

    Create TodoList and SingleTodo components.

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className='input'>
-      <input type='text' placeholder='Enter a task' className='input__box' />
+    <form
+      className='input'
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type='text'
+        placeholder='Enter a task'
+        className='input__box'
+        value={todo}
+        ref={inputRef}
+        onChange={(e) => setTodo(e.target.value)}
+      />
       <button type='submit' className='input__submit'>
         Go
       </button>
